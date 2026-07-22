a1dbd6ef36143df44eed037a5fe313bc7a71a89f
main <mehwish01012001@gmail.com>
2026-07-22 11:55:16 +0500

    dddddd

--- services/mailTransporter.js
+++ services/mailTransporter.js
@@ -1,21 +1,26 @@
 const nodemailer = require("nodemailer");
+const dns = require("dns");
+
+const emailHost = process.env.EMAIL_HOST || "smtp.gmail.com";
+const emailPort = Number(process.env.EMAIL_PORT) || 465;
+const emailSecure = process.env.EMAIL_SECURE === "true" || emailPort === 465;
+const emailUser = process.env.EMAIL_USER;
+const emailPass = process.env.EMAIL_PASS || process.env.EMAIL_PASSWORD;
 
 const transporter = nodemailer.createTransport({
-
-    host: "smtp.gmail.com",
-
-    port: 465,
-
-    secure: true,
-
-    family: 4,
-
-    auth:{
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
-
+  host: emailHost,
+  port: emailPort,
+  secure: emailSecure,
+  auth: {
+    user: emailUser,
+    pass: emailPass,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+  family: 4,
+  connectionTimeout: 10000,
+  lookup: (hostname, options, callback) => dns.lookup(hostname, { family: 4 }, callback),
 });
-
 
 module.exports = transporter;
